cd9fae61dbd983395b61e9c1de01060c9f67156b
marianaceotto <marianaceott@gmail.com>
2022-12-20 13:39:58 -0300

    Part3 - Details e SignUp

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,3 +1,4 @@
+import { Flex, Heading } from '@chakra-ui/react'
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -5,36 +6,36 @@ import Header from '../../components/Header'
 import { BASE_URL } from '../../constants/url'
 import { GlobalContext } from '../../contexts/GlobalContext'
 import { goToLoginPage } from '../../routes/coordinator'
-import { Flex, Heading } from '@chakra-ui/react'
-import RecipeCard from "./RecipeCard"
+import RecipeCard from './RecipeCard'
 
 const HomePage = () => {
+
     const context = useContext(GlobalContext)
-    const navigate = useNavigate()
+    const navigate = useNavigate([])
 
     const [ recipes, setRecipes ] = useState([])
 
-   
     useEffect(() => {
         if (!context.isAuth) {
             goToLoginPage(navigate)
         }
-    }, [])
+    })
 
     useEffect(() => {
         fetchRecipes()
-    }, [])
+    })
 
     const fetchRecipes = async () => {
         try {
-            const config = {
-                headers: {
-                    Authorization: window.localStorage.getItem("cookenu-token")
-                }
-            }
 
+            // const config = {
+            //     headers: {
+            //         Authorization: window.localStorage.getItem("cookenu-token")
+            //     }
+            // }
             const response = await axios.get(`${BASE_URL}/recipe/all`, config)
             setRecipes(response.data)
+
         } catch (error) {
             console.log(error)
         }
@@ -44,7 +45,7 @@ const HomePage = () => {
         <>
             <Header />
             <Flex flexDir="column" paddingTop={5}>
-                <Heading>Receitas</Heading>
+                <Heading>Receitas Cookenu</Heading>
                 <Flex flexWrap={"wrap"} justifyContent="space-between">
                     {recipes.map((recipe) => {
                         return <RecipeCard key={recipe.id} recipe={recipe} />
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,25 +1,24 @@
-import {
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-  Button,
-  Heading,
-  Text,
-  useColorModeValue,
-  Spinner,
-  Link
-} from '@chakra-ui/react';
-
 import { useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { BASE_URL } from "../../constants/url"
-import { goToHomePage } from "../../routes/coordinator"
-import { goToSignupPage } from "../../routes/coordinator"
+import { goToSignupPage, goToHomePage } from "../../routes/coordinator"
 import { GlobalContext } from '../../contexts/GlobalContext';
+
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Spinner,
+    Link
+  } from '@chakra-ui/react';
 
 
 const LoginPage = () => {
@@ -35,16 +34,17 @@ const LoginPage = () => {
   })
 
   useEffect(() => {
-      if (!context.isAuth) {
+      if (context.isAuth) {
           goToHomePage(navigate)
       }
-  })
+  }, [context.isAuth, navigate])
 
   const onChangeForm = (event) => {
       setForm({ ...form, [event.target.name]: event.target.value })
   }
 
   const login = async () => {
+
       try {
           setIsLoading(true)
 
@@ -54,8 +54,7 @@ const LoginPage = () => {
           }
 
           const response = await axios.post(
-              `${BASE_URL}/user/login`,
-              body
+              `${BASE_URL}/user/login`, body
           )
 
           window.localStorage.setItem("cookenu-token", response.data.token)
@@ -63,6 +62,7 @@ const LoginPage = () => {
           context.setIsAuth(true)
 
           goToHomePage(navigate)
+
       } catch (error) {
           console.log(error)
           setIsLoading(false)
@@ -71,69 +71,69 @@ const LoginPage = () => {
 
 
   return (
-      <Flex
-          minH={'100vh'}
-          align={'center'}
-          justify={'center'}
-          bg={useColorModeValue('gray.50', 'gray.800')}>
-          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-              <Stack align={'center'}>
-                  <Heading fontSize={'4xl'}>Entre em sua conta</Heading>
-                  <Text fontSize={'lg'} color={'gray.600'}>
-                      para aproveitar as melhores receitas Cookenu ✌️
-                  </Text>
-              </Stack>
-              <Box
-                  rounded={'lg'}
-                  bg={useColorModeValue('white', 'gray.700')}
-                  boxShadow={'lg'}
-                  p={8}>
-                  <Stack spacing={4}>
-                      <FormControl id="email">
-                          <FormLabel>Email</FormLabel>
-                          <Input
-                              type="email"
-                              value={form.email}
-                              onChange={onChangeForm}
-                              name="email"
-                              autoComplete='off'
-                          />
-                      </FormControl>
-                      <FormControl id="password">
-                          <FormLabel>Senha</FormLabel>
-                          <Input
-                              type="password"
-                              value={form.password}
-                              onChange={onChangeForm}
-                              name="password"
-                          />
-                      </FormControl>
-                      <Stack spacing={10}>
-                          <Button
-                              bg={'blue.400'}
-                              color={'white'}
-                              _hover={{
-                                  bg: 'blue.500',
-                              }}
-                              onClick={login}
-                          >
-                              {isLoading ? <Spinner /> : "Entrar"}
-                          </Button>
-                      </Stack>
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Entre em sua conta</Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        para aproveitar as melhores receitas Cookenu ✌️
+                    </Text>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <Stack spacing={4}>
+                        <FormControl id="email">
+                            <FormLabel>Email</FormLabel>
+                            <Input
+                                type="email"
+                                value={form.email}
+                                onChange={onChangeForm}
+                                name="email"
+                                autoComplete='off'
+                            />
+                        </FormControl>
+                        <FormControl id="password">
+                            <FormLabel>Senha</FormLabel>
+                            <Input
+                                type="password"
+                                value={form.password}
+                                onChange={onChangeForm}
+                                name="password"
+                            />
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Button
+                                bg={'blue.400'}
+                                color={'white'}
+                                _hover={{
+                                    bg: 'blue.500',
+                                }}
+                                onClick={login}
+                            >
+                                {isLoading ? <Spinner /> : "Entrar"}
+                            </Button>
+                        </Stack>
 
-                      <Stack paddingTop={5} paddingBottom={5}>
-                          <Text textAlign={"center"}>
-                              Ainda não tem conta? {" "}
-                              <Link color="blue" onClick={() => goToSignupPage(navigate)}>
-                                  Cadastre-se!
-                              </Link>
-                          </Text>
-                      </Stack>
-                  </Stack>
-              </Box>
-          </Stack>
-      </Flex>
-  )
+                        <Stack paddingTop={5} paddingBottom={5}>
+                            <Text textAlign={"center"}>
+                                Ainda não tem conta? {" "}
+                                <Link color="blue" onClick={() => goToSignupPage(navigate)}>
+                                    Cadastre-se!
+                                </Link>
+                            </Text>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
+    )
 }
 
 export default LoginPage
